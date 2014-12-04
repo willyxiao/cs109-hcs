495d4264c58a59ee1efa07fb20f0968465d381f9
Anson Kahng <ansonkahng@gmail.com>
2014-12-04 01:44:18 -0500

    exclamations in mbox to bow

--- mbox_to_bow.py
+++ mbox_to_bow.py
@@ -12,13 +12,14 @@ def make_bow(mbox):
 	for msg in mbox:
 		try:
 			body = get_body(msg)
-			print body
+			print count_exclamations(body)
 		except:
 			logging.error('Could not get body of email' + str(msg['Subject']))
 			continue
 
 def get_body(msg):
 	body = None
+	# step through until not multipart; take that part as text
 	if msg.is_multipart():
 		for part in msg.walk():
 			if part.is_multipart():
@@ -27,6 +28,10 @@ def get_body(msg):
 						body = subpart.get_payload(decode=True)
 			elif part.get_content_type() == 'text/plain':
 				body = part.get_payload(decode=True)
+	# if not multipart to begin with, take payload as text
 	elif msg.get_content_type() == 'text/plain':
 		body = msg.get_payload(decode=True)
 	return body
+
+def count_exclamations(body):
+	return body.count('!')
