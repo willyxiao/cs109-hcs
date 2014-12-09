abd372206deac3b996ecf5488c0784cb06e857d6
Anson Kahng <ansonkahng@gmail.com>
2014-12-09 18:01:25 -0500

    predict v3

--- mbox_to_bow.py
+++ mbox_to_bow.py
@@ -342,6 +342,7 @@ def evaluate_classifiers(classifiers,global_bow_words,input_message):
 	
 	# transform input message to matrix
 	input_bow_mat = make_bow_given_dict_string(input_message, global_bow_words)
+	print input_bow_mat
 
 	for classifier in classifiers:
 		print classifier.predict(input_bow_mat)
