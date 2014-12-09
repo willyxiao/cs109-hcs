b662b8f3bc5b6946ca1d220881cf50b37d50665f
Anson Kahng <ansonkahng@gmail.com>
2014-12-08 22:30:25 -0500

    bow

--- mbox_to_bow.py
+++ mbox_to_bow.py
@@ -6,7 +6,7 @@ import math
 import logging
 import collections
 
-def make_bow(mbox):#,num_words):
+def make_bow(mbox,num_words):
 	if type(mbox) is str:
 		mbox = mailbox.mbox(mbox)
 
@@ -29,8 +29,9 @@ def make_bow(mbox):#,num_words):
 			continue
 
 	global_bow = collections.Counter(global_text)
-	print len(global_bow)
-	# global_bow_top = global_bow.most_common(num_words)
+	# 14677 unique words
+	global_bow_top = global_bow.most_common(num_words)
+	print global_bow_top
 
 	# return global_bow
 
