869b6caf67ce537267e122ad84b0196c16325b2d
Willy Xiao <willyxiao@gmail.com>
2014-12-09 16:26:13 -0500

    edited graph_json script

--- graph_json.py
+++ graph_json.py
@@ -2,20 +2,20 @@ import json
 import mbox_to_graph
 import sys
 
-inpath = "/mnt/archive/"
+inpath = "/mnt/archives/"
 
 outpath = "webpage/"
 
-# takes the name of the list (i.e. without the '/mnt/archive')
-def create_json(mlist):
-	full_path = inpath + mlist + ".mbox"
-	data = mbox_to_graph.make_graph(full_path)
-	with open(outpath + mlist + ".json", "w") as outfile:
+# takes the name of the list (i.e. without the '/mnt/archives/')
+def create_json(mlist, year):
+        full_path = inpath + mlist + ".mbox"
+	data = mbox_to_graph.make_graph(full_path, year)
+	with open(outpath + "{0}-{1}.json".format(mlist, year), "w") as outfile:
 		json.dump(data, outfile)
 
 if __name__ == "__main__":
-	if (len(sys.argv) < 2):
-		print "Usage: {0} <list>".format(sys.argv[0])
+	if (len(sys.argv) < 3):
+		print "Usage: {0} <list> <year>".format(sys.argv[0])
 		sys.exit(1)
 
-	create_json(sys.argv[1])
+	create_json(sys.argv[1], sys.argv[2])
