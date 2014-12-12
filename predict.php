912df4d3a438b5c6e7c84a553d62f27628c31ca4
Willy Xiao <willyxiao@gmail.com>
2014-12-12 15:00:26 -0500

    predict.php

--- predict.php
+++ predict.php
@@ -2,6 +2,7 @@
 
 
 	// print exec("python predict_message.py " + $_POST["email"]);
-	print exec("python predict_message.py " + "'hi bye hi lie hi die hi why hi sly'");
+	$cmd = "/usr/bin/python predict_message.py 'hi bye hi lie hi die hi why hi sly'";
+  print `$cmd`;
 
 ?>
