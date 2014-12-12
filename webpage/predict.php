2ea9e45890e144d7691067e50b6b1f07c4e94ce7
Willy Xiao <willyxiao@gmail.com>
2014-12-12 15:24:03 -0500

    predict.php dynamic

--- webpage/predict.php
+++ webpage/predict.php
@@ -2,7 +2,7 @@
 
 
 	// print exec("python predict_message.py " + $_POST["email"]);
-	$cmd = "/usr/bin/python predict_message.py 'hi bye hi lie hi die hi why hi sly'";
+	$cmd = "/usr/bin/python predict_message.py '" + $_POST["email"] + "'";
   print `$cmd`;
 
 ?>
