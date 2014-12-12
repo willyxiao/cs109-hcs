1eeadc4951e46749fd740cb8fba10d874ade2a4a
Anson Kahng <ansonkahng@gmail.com>
2014-12-12 14:50:33 -0500

    php

--- predict.php
+++ predict.php
@@ -1,6 +1,7 @@
 <?php 
 
 
-	print exec("python predict_message.py " + $_POST["email"]);
+	// print exec("python predict_message.py " + $_POST["email"]);
+	print exec("python predict_message.py " + "'hi bye hi lie hi die hi why hi sly'");
 
 ?>
